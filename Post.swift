cb2c8b0a1e244efa9ae0cf1875e655be42c1a809
aidansussillo24 <aidanmsussillo@gmail.com>
2025-06-25 18:21:19 -0400

    Convert temperature to Fahrenheit

--- Post.swift
+++ Post.swift
@@ -72,10 +72,11 @@ struct Post: Identifiable, Codable {
         }
     }
 
-    /// Temperature as a rounded string with degree symbol
+    /// Temperature in Fahrenheit as a rounded string with degree symbol
     var tempString: String? {
-        guard let t = temp else { return nil }
-        return String(format: "%.0f\u{00B0}", t)
+        guard let c = temp else { return nil }
+        let f = c * 9 / 5 + 32
+        return String(format: "%.0f\u{00B0}", f)
     }
 
     enum CodingKeys: String, CodingKey {
